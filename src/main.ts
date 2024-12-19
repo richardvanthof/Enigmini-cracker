3fd156593fddb67879770f5c17ae6378ada0b0d6
Richard van 't Hof <hello@therichard.space>
2024-12-19 18:07:47 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './style.css'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Enigmini</h1>
+    <div class="card">
+    </div>
+    <p class="read-the-docs">
+      Open browser console to see script output.
+    </p>
+  </div>
+`
+
+
